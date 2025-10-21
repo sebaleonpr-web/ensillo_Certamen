44a8ae12cef0a6dba29aa7d02979d6c0b8c684b9
{Seba} <sebaleonpr@gmail.com>
2025-10-21 15:18:14 -0300

    commit_v51

--- enso_Certamen/obj/Debug/net9.0/enso_Certamen.AssemblyInfo.cs
+++ enso_Certamen/obj/Debug/net9.0/enso_Certamen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("enso_Certamen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+322aa487ab60397f8b56be918ce2d17e7d3cadac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e1e0bc71ca296ecbb054b665cddee448c49af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("enso_Certamen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("enso_Certamen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
